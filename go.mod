edc0fc29b967543af995ed3221d4fa3239e00f84
Sebastián Gallardo <iguesimadev@gmail.com>
2026-08-06 20:22:13 -0400

    first commit

---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/iGuessImaDev/gocourse_user
+
+go 1.24.1
+
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/iGuessImaDev/gocourse_domain v0.0.1
+	github.com/iGuessImaDev/gocourse_meta v0.0.1
+	gorm.io/gorm v1.31.2
+)
+
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	golang.org/x/text v0.20.0 // indirect
+)
---
+++ user/service.go
@@ -0,0 +1,86 @@
+package user
+
+import (
+	"log"
+
+	"github.com/iGuessImaDev/gocourse_domain/domain"
+)
+
+type (
+	Filters struct {
+		FirstName string
+		LastName  string
+	}
+
+	Service interface {
+		Create(firstName, lastName, email, phone string) (*domain.User, error)
+		GetAll(filter Filters, offset, limit int) ([]domain.User, error)
+		Get(id string) (*domain.User, error)
+		Delete(id string) error
+		Update(id string, firstName *string, lastName *string, email *string, phone *string) error
+		Count(filters Filters) (int, error)
+	}
+
+	service struct {
+		log  *log.Logger
+		repo Repository
+	}
+)
+
+func NewService(log *log.Logger, repo Repository) Service {
+	return &service{
+		log:  log,
+		repo: repo,
+	}
+}
+
+func (s service) Create(firstName, lastName, email, phone string) (*domain.User, error) {
+	s.log.Println("create user service")
+	user := domain.User{
+		FirstName: firstName,
+		LastName:  lastName,
+		Email:     email,
+		Phone:     phone,
+	}
+	if err := s.repo.Create(&user); err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (s service) GetAll(filters Filters, offset, limit int) ([]domain.User, error) {
+	s.log.Println("getall user service")
+	users, err := s.repo.GetAll(filters, offset, limit)
+	if err != nil {
+		s.log.Println(err)
+		return nil, err
+	}
+	return users, nil
+}
+
+func (s service) Get(id string) (*domain.User, error) {
+	s.log.Println("get user service")
+	user, err := s.repo.Get(id)
+	if err != nil {
+		s.log.Println(err)
+		return nil, err
+	}
+	return user, nil
+}
+
+func (s service) Delete(id string) error {
+	err := s.repo.Delete(id)
+	if err != nil {
+		s.log.Println(err)
+		return err
+	}
+	return nil
+}
+
+func (s service) Update(id string, firstName *string, lastName *string, email *string, phone *string) error {
+	return s.repo.Update(id, firstName, lastName, email, phone)
+}
+
+func (s service) Count(filters Filters) (int, error) {
+	return s.repo.Count(filters)
+}
